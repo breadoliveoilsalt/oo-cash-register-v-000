682f75cc8eb62eb77d6d6b2a01b1212bce6f79e6
Tony Distinti <adistinti@gmail.com>
2017-09-28 15:14:04 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -16,11 +16,11 @@ class CashRegister
 
   def apply_discount
     if @discount == nil
-      puts "There is no discount to apply."
+      "There is no discount to apply."
     else
       discount_percentage = (100-@discount)/100.to_f
       @total *= discount_percentage
-      puts "After the discount, the total comes
+      "After the discount, the total comes
  to #{@total.to_i}."
     end
         #  binding.pry
